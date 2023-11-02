95618d8f52e3d648216cefbde83a5d932a0bb3ff
Sergey Oldyrev <toryn_h.e@mail.ru>
2023-11-02 20:15:17 +0300

    add dgrag annd drop test

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,9 +18,11 @@ public class DragAndDropTest {
     void successfulDragAndDrop() {
 
         open("/drag_and_drop");
-        $("ul.d-lg-flex.list-style-none").$(byText("Solutions")).hover();
-        $("[href='/enterprise']").click();
-        $("#hero-section-brand-heading").shouldBe(visible);
-        $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform"));
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+        $("#column-b").dragAndDropTo("#column-a");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
